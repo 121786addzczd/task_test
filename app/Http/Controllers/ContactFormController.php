932428ecb50d0c1ca9ddfea20c03c14e2146d911
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2022-10-24 05:47:10 +0900

    編集画面作成

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -98,7 +98,10 @@ class ContactFormController extends Controller
      */
     public function edit($id)
     {
-        //
+        $contact = ContactForm::find($id);
+
+        return view('contacts.edit', compact('contact'));
+
     }
 
     /**
